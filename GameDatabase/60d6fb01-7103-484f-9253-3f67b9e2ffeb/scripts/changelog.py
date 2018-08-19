89154875fa2be5b69421470bae0d689dc88e32fe
brine <octgngamedirectory@gmail.com>
2018-08-19 16:49:51 +1000

    changelog update for v3

--- GameDatabase/60d6fb01-7103-484f-9253-3f67b9e2ffeb/scripts/changelog.py
+++ GameDatabase/60d6fb01-7103-484f-9253-3f67b9e2ffeb/scripts/changelog.py
@@ -1,5 +1,7 @@
 changelog = {
-    0000100: ("0.0.1.1", "16 August 2018", [
-        "Welcome to the Keyforge OCTGN Plugin!"
+    0000103: ("0.0.1.3", "19 August 2018", [
+        "Welcome to the Keyforge OCTGN Plugin!",
+        "This is still a work-in-progress, and is missing data for  many cards.",
+        "Please join the OCTGN discord server to discuss development with us!"
         ])
 }
